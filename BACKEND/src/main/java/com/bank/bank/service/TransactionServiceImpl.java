bd6ac8475a0705146b49f9abbda1ef5f9fe27ff1
mathias labora martirena <mathias.labora@sofka.com.co>
2024-03-27 17:54:01 -0300

    feat: funciones en transaction service

--- BACKEND/src/main/java/com/bank/bank/service/TransactionServiceImpl.java
+++ BACKEND/src/main/java/com/bank/bank/service/TransactionServiceImpl.java
@@ -21,7 +21,20 @@ public class TransactionServiceImpl implements TransactionService{
 
     @Override
     public Mono<Transaction> newTransaction(Transaction transaction) {
-        return Mono.just(transactionRepository.save(transaction));
+        switch (transaction.getTransaction_type()) {
+            case "Compra":
+                return CardPurchase(transaction);
+            case "Transferencia":
+                return Transfer(transaction);
+            case "Depósito":
+                return Deposit(transaction);
+            case "Retiro":
+                return Withdrawal(transaction);
+            case "PagoFactura":
+                return BillPayment(transaction);
+            default:
+                return Mono.error(new UnsupportedOperationException("Tipo de transacción no especificada"));
+        }
     }
 
     @Override
@@ -35,5 +48,38 @@ public class TransactionServiceImpl implements TransactionService{
             transactionRepository.deleteById(Long.valueOf(transactionId));
             return true;
         });
+    }
+
+
+
+    private Mono<Transaction> CardPurchase(Transaction transaction) {
+        if (transaction.getOrdinal() > 1) {
+            Flux.range(1, transaction.getOrdinal())
+                    .flatMap(i -> {
+                        Transaction newTransaction = transaction;
+                        newTransaction.setOrdinal(i.shortValue());
+                        return Mono.just(transactionRepository.save(newTransaction));
+                    });
+        } else {
+             Mono.just(transactionRepository.save(transaction));
+        }
+
+        return Mono.just(transactionRepository.save(transaction));
+    }
+
+    private Mono<Transaction> Transfer(Transaction transaction) {
+        return Mono.just(transaction);
+    }
+
+    private Mono<Transaction> Deposit(Transaction transaction) {
+        return Mono.just(transaction);
+    }
+
+    private Mono<Transaction> Withdrawal(Transaction transaction) {
+        return Mono.just(transaction);
+    }
+
+    private Mono<Transaction> BillPayment(Transaction transaction) {
+        return Mono.just(transaction);
     }
 }
