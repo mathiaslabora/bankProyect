6f8776ddbf9b038bf376eb840ba5d05613a2a225
mathias labora martirena <mathias.labora@sofka.com.co>
2024-03-31 18:48:31 -0300

    fix: cambios en main para railway cors correccion 5

--- BACKEND/src/main/java/com/bank/bank/CorsConfig.java
+++ BACKEND/src/main/java/com/bank/bank/CorsConfig.java
@@ -13,7 +13,11 @@ public class CorsConfig implements WebMvcConfigurer {
 
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOriginPatterns("*").allowedMethods("*").allowedHeaders("*");
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
             }
 
 
