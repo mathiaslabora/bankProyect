e6b9d7df7eda5fb995f4508f06d8414516e3ab03
mathias labora martirena <mathias.labora@sofka.com.co>
2024-03-31 17:58:49 -0300

    cambios para railway

--- BACKEND/src/main/java/com/bank/bank/MathiasbankApplication.java
+++ BACKEND/src/main/java/com/bank/bank/MathiasbankApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication()
 public class MathiasbankApplication {
@@ -13,4 +16,13 @@ public class MathiasbankApplication {
 		SpringApplication.run(MathiasbankApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("/**");
+			}
+		};
+	}
 }
